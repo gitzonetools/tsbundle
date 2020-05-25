b418ccd10a50d5a17bf5c859b9810326d4b4fdc3
Phil Kunz <gitlab@philkunz.com>
2020-05-25 17:01:43 +0000

    fix(core): update

--- ts/tsbundle.class.tsbundle.ts
+++ ts/tsbundle.class.tsbundle.ts
@@ -43,11 +43,7 @@ export class TsBundle {
         // which external modules to include in the bundle
         // https://github.com/rollup/rollup-plugin-node-resolve#usage
         plugins.rollupResolve(),
-        plugins.rollupCommonjs({
-          namedExports: {
-            'node_modules/@pushrocks/smartstate/dist/index.js': ['Smartstate']
-          }
-        }),
+        plugins.rollupCommonjs({}),
 
         // Resolve source maps to the original source
         plugins.rollupSourceMaps()
