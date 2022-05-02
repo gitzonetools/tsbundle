d788bdc78b84e360aaed6ca22c6d32af870821d3
Philipp Kunz <gitlab@philkunz.com>
2022-05-02 18:23:36 +0200

    fix(core): update

---
+++ ts/00_commitinfo_data.ts
@@ -0,0 +1,8 @@
+/**
+ * autocreated commitinfo by @pushrocks/commitinfo
+ */
+export const commitinfo = {
+  name: '@gitzone/tsbundle',
+  version: '1.0.103',
+  description: 'a bundler using rollup for painless bundling of web projects'
+}
