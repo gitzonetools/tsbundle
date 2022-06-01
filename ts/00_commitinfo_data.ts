daba025307cbb3313b5100a697d57d8690caa866
Philipp Kunz <gitlab@philkunz.com>
2022-06-02 01:00:49 +0200

    fix(core): update

--- ts/00_commitinfo_data.ts
+++ ts/00_commitinfo_data.ts
@@ -3,6 +3,6 @@
  */
 export const commitinfo = {
   name: '@gitzone/tsbundle',
-  version: '2.0.4',
+  version: '2.0.5',
   description: 'a bundler using rollup for painless bundling of web projects'
 }
